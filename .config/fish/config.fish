d8375cfdd0639149daf95ec2a07bb1e1854a35f6
Matt Cuento <cuentomr17@gmail.com>
2026-07-31 15:19:59 -0700

    Add JJ workflow aliases and abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,6 +17,34 @@ abbr -a -- top btm
 abbr -a -- du dust
 alias ll='eza -lbG --git'
 
+# Additional jj abbreviations adapted from Oliver Nguyen's workflow.
+# Fisher's kapsmudit/plugin-jj provides the rest of the `j*` abbreviations.
+abbr -a -- jab 'jj absorb'
+abbr -a -- 'jn+' 'jj new -A @'
+abbr -a -- 'jn-' 'jj new -B @'
+abbr -a -- jrs 'jj restore'
+abbr -a -- js 'jj status'
+abbr -a -- jss 'jj show @ --summary'
+abbr -a -- jjs 'jj show @ --summary'
+abbr -a -- 'j+' 'jj next'
+abbr -a -- 'j++' 'jj next 2'
+abbr -a -- 'j+++' 'jj next 3'
+abbr -a -- 'j++++' 'jj next 4'
+abbr -a -- 'j+2' 'jj next 2'
+abbr -a -- 'j+3' 'jj next 3'
+abbr -a -- 'j+4' 'jj next 4'
+abbr -a -- 'j+5' 'jj next 5'
+abbr -a -- 'j+6' 'jj next 6'
+abbr -a -- 'j-' 'jj prev'
+abbr -a -- 'j--' 'jj prev 2'
+abbr -a -- 'j---' 'jj prev 3'
+abbr -a -- 'j----' 'jj prev 4'
+abbr -a -- 'j-2' 'jj prev 2'
+abbr -a -- 'j-3' 'jj prev 3'
+abbr -a -- 'j-4' 'jj prev 4'
+abbr -a -- 'j-5' 'jj prev 5'
+abbr -a -- 'j-6' 'jj prev 6'
+
 # Abbreviations for nextest
 alias ct='cargo nextest run'
 alias ctp='cargo nextest run -p'
