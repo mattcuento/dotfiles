382355d371ff42088b58ca310797359d4ca59d9b
Matt Cuento <cuentomr17@gmail.com>
2026-08-15 22:16:52 -0700

    Enable live Rust diagnostics with bacon-ls

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,10 @@
 vim.opt.termguicolors = true
 vim.o.background = "dark"
 
+-- Keep rust-analyzer for language intelligence, but delegate compiler and
+-- Clippy diagnostics to bacon-ls so unsaved buffers can be checked.
+vim.g.lazyvim_rust_diagnostics = "bacon-ls"
+
 -- ============================================================================
 -- OPTIONS
 -- ============================================================================
