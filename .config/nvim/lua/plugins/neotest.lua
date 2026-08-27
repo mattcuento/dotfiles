ba23cfb815d1847568696a9a082044b98ee40c07
Matt Cuento <cuentomr17@gmail.com>
2026-08-27 15:31:23 -0400

    Remove Neotest output panel override

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,16 +1,1 @@
-return {
-    {
-        "nvim-neotest/neotest",
-        keys = {
-            {
-                "<leader>tt",
-                function()
-                    local neotest = require("neotest")
-                    neotest.output_panel.open()
-                    neotest.run.run(vim.fn.expand("%"))
-                end,
-                desc = "Run File (Neotest)",
-            },
-        },
-    },
-}
+return {}
